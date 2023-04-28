8917cf8c6317d91ebf800c140d47cb85803ec834
himavanthreddy94 <himavanth94@gmail.com>
2023-04-28 16:56:56 +0530

    second commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import {useState} from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+
+    const [mobile, setMobile] = useState(false);
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Navbar
